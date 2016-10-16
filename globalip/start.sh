d2cef2ce2bbde7839cbcbae17d189e93983bf266
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-10-17 00:30:23 +0900

    Update start.sh

--- globalip/start.sh
+++ globalip/start.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 set -e
 
-/root/google-cloud-sdk/bin/gcloud auth activate-service-account $GCLOUD_ACCOUNT --key-file $GCLOUD_KEY_FILE
+/root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $GCLOUD_KEY_FILE
 
 echo "$CRONTAB_TIME /root/insert.sh" >> /etc/crontabs/root
 crond -f
