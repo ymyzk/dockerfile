70a1a4518145ded86f7e4b2c3129775c2b7ec109
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2020-05-03 18:12:09 +0900

    Remove globalip

--- globalip/insert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-time=$(date +"%s")
-ip=$(curl -s https://httpbin.org/ip | jq .origin)  # value is double-quoted
-json="{\"time\": ${time}, \"address\": ${ip}}"
-
-echo $json
-echo $json | /root/google-cloud-sdk/bin/bq insert $BQ_TABLE
--- globalip/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-set -e
-
-/root/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $GCLOUD_KEY_FILE
-
-echo "$CRONTAB_TIME /root/insert.sh" >> /etc/crontabs/root
-crond -f
